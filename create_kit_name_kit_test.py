2ae6167b248364496c2d17795be1339b2bd287af
Vladimir <vladimirfdez2016@gmail.com>
2024-11-07 23:02:02 +0100

    Nueva versión del proyecto para Sprint 7, considerando las correcciones sugeridas

--- create_kit_name_kit_test.py
+++ create_kit_name_kit_test.py
@@ -9,10 +9,14 @@ def negative_assert(response, expected_status=400):
 def test_1_1_letter_name():
     response = sender_stand_request.post_new_client_kit({"name": "a"})
     positive_assert(response)
+    response_data = response.json()
+    assert response_data["name"] == "a", "Error: el nombre del kit en la respuesta no coincide con el nombre enviado en la solicitud."
 
 def test_2_511_letter_name():
     response = sender_stand_request.post_new_client_kit({"name": "AbcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdAbcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabC"})
     positive_assert(response)
+    response_data = response.json()
+    assert response_data["name"] == "AbcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdAbcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabC", "Error: el nombre del kit en la respuesta no coincide con el nombre enviado en la solicitud."
 
 def test_3_0_letter_name():
     response = sender_stand_request.post_new_client_kit({"name": ""})
@@ -24,15 +28,21 @@ def test_4_512_letter_name():
 
 def test_5_special_car_name():
     response = sender_stand_request.post_new_client_kit({"name": "№%@"})
-    negative_assert(response, 400)
+    positive_assert(response)
+    response_data = response.json()
+    assert response_data["name"] == "№%@", "Error: el nombre del kit en la respuesta no coincide con el nombre enviado en la solicitud."
 
 def test_6_space_bt_letters_name():
     response = sender_stand_request.post_new_client_kit({"name": " A Aaa "})
     positive_assert(response)
+    response_data = response.json()
+    assert response_data["name"] == " A Aaa ", "Error: el nombre del kit en la respuesta no coincide con el nombre enviado en la solicitud."
 
 def test_7_string_number_name():
     response = sender_stand_request.post_new_client_kit({"name": "123"})
     positive_assert(response)
+    response_data = response.json()
+    assert response_data["name"] == "123", "Error: el nombre del kit en la respuesta no coincide con el nombre enviado en la solicitud."
 
 def test_8_no_name():
     response = sender_stand_request.post_new_client_kit({})
@@ -41,4 +51,3 @@ def test_8_no_name():
 def test_9_integer_name():
     response = sender_stand_request.post_new_client_kit({"name": 123})
     negative_assert(response, 400)
-
